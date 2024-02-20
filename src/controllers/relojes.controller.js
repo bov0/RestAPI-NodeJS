6439d8cd042bc3f6e1d6e850fd8a1595f1f5dbda
bov0 <ibay2002@gmail.com>
2024-02-20 12:11:30 +0100

    first commit

--- src/controllers/relojes.controller.js
+++ src/controllers/relojes.controller.js
@@ -38,7 +38,7 @@ const obtenerReloj = async (req, res) => {
 const agregarReloj = async (req, res) => {
     try {
         const { modelo, precio, id_marca } = req.body;
-        const imagen = req.file ? saveImage(req.file) : null;
+        const imagen = req.file ? req.file.originalname : null;
 
         if (!modelo || modelo.trim() === "") {
             return res.status(400).json({ message: "Solicitud incorrecta, 'modelo' es obligatorio y no puede estar vacío" });
@@ -59,7 +59,7 @@ const agregarReloj = async (req, res) => {
             return res.status(400).json({ message: "Solicitud incorrecta, 'id_marca' no existe" });
         }
 
-        const imagenblob = req.file ? req.file.path : null;
+        const imagenblob = req.file ? guardarImagen(req.file) : null;
 
         const reloj = { modelo, precio, imagen, imagenblob, id_marca };
         await connection.query("INSERT INTO relojes SET ?", reloj);
@@ -81,9 +81,10 @@ const actualizarReloj = async (req, res) => {
             return res.status(400).json({ message: "Solicitud incorrecta, 'id_marca' no existe" });
         }
 
-        const imagen = req.file ? saveImage(req.file) : null;
+        const imagen = req.file ? req.file.originalname : null;
         const imagenblob = req.file ? req.file.path : null;
         const { id } = req.params;
+
         // Cuando se suba a vercel comentar esta linea
 
         if (!modelo || modelo.trim() === "") {
@@ -112,17 +113,29 @@ const eliminarReloj = async (req, res) => {
     try {
         const { id } = req.params;
         const connection = await getConnection();
-        const resultado = await connection.query("DELETE FROM relojes WHERE id = ?", id);
-        res.json(resultado);
+        
+        // Utiliza [0] para acceder al valor del resultado
+        const buscarID = (await connection.query("SELECT COUNT (*) FROM relojes WHERE id = ?", id))[0]['COUNT (*)'];
+
+        if (buscarID > 0) {
+            const resultado = await connection.query("DELETE FROM relojes WHERE id = ?", id);
+            res.json({ message: "Eliminado reloj con id " + id + " correctamente" });
+        } else {
+            res.json({ message: "No se ha encontrado reloj con id " + id });
+        }
+        
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
 
-function saveImage(file) {
-    const newPath = `./static/img/${file.originalname}`;
-    fs.renameSync(file.path, newPath);
-    return newPath;
+function guardarImagen(img) {
+    //añadir sufijo para no tener problemas subiendo la misma imagen en local
+    const prefijo = Date.now();
+    const nombreImagen = `${prefijo}-${img.originalname}`;
+    const ruta = `./static/img/${nombreImagen}`;
+    fs.renameSync(img.path, ruta);
+    return ruta;
 }
 
 // Ruta para manejar la subida de archivos
@@ -134,7 +147,7 @@ const subirImagenReloj = async (req, res, next) => {
             } else if (err) {
                 return res.status(500).json({ message: "Error al procesar la solicitud" });
             }
-            next();
+            next(); //parecido a un break
         });
     } catch (error) {
         console.error(error);
